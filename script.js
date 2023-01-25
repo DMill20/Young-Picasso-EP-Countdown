468471a7c31dcfd4bb33c1f038d7984e834a363d
Diamond M <89270895+DMill20@users.noreply.github.com>
2023-01-25 17:00:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ function getRandomInt(min, max) {
   particleInit();
  
 
-var countDownDate = new Date("Nov 18, 2022 00:00:00").getTime();
+var countDownDate = new Date("Jan 15, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
@@ -60,7 +60,7 @@ var x = setInterval(function () {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("demo").innerHTML = "";
   }
 }, 1000);
  
